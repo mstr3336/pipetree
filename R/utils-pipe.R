71be11e4afa36779069933777a912c63512f83ba
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-28 20:18:04 +1100

    Export Backpipe

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -9,3 +9,16 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
+
+#' Backpipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+NULL
+
