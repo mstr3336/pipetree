457e8a6f33f0b1ad2c0e7b86b0410f5543ecb0bb
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-02-04 17:26:55 +1100

    Update doc helper

--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,19 @@ get_input <- function(prompt = "") {
 #' Describe a function that produces a list
 #' @keywords internal
 #' @export
-describe_list_function <- function(fn) {
-  return(fn() %>% yaml::as.yaml())
+describe_list_function <- function(fn, section_name = "List Section") {
+  input <- fn()
+  g <- glue::glue
+
+  out <- input %>% yaml::as.yaml()
+
+  out <- g(
+    "@section {section_name}",
+    "```yaml",
+    "{out}",
+    "```",
+    .sep = "\n"
+  )
+
+  return(out)
 }
