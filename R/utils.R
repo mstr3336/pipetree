79ad84d7890682747c74a124c53676b27477f3e3
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-02-04 17:24:18 +1100

    Export helper

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,9 @@ get_input <- function(prompt = "") {
   return(readline(prompt = prompt))
 }
 
-
+#' Describe a function that produces a list
+#' @keywords internal
+#' @export
 describe_list_function <- function(fn) {
   return(fn() %>% yaml::as.yaml())
 }
